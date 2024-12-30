a48f468151ac92dfaba1fd2db98bb13c1db702cb
Gabi Alvarez <gabrielalvarez@dorna.com>
2024-12-30 16:34:45 +0100

    feat(pwatest): :construction_worker: main
    PWA config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,77 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  modules: ["@nuxt/image", "@vite-pwa/nuxt", "@nuxt/devtools"],
+  
   routeRules: {
     // prerender index route by default
-    '/': { prerender: true },
+    "/": { prerender: true },
+  },
+  pwa: {
+
+    registerType: "autoUpdate",
+    manifest: {
+      name: "GabiPWATest",
+      short_name: "GabiPWATest",
+      theme_color: "#dddddd",
+      background_color: "#000000",
+      display: "standalone",
+      icons: [
+        {
+          src: "/icons/pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png",
+        },
+        {
+          src: "/icons/pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/icons/pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "/icons/maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+
+    //NOTA:Experimentalhttps://vite-pwa-org.netlify.app/frameworks/nuxt.html =>>
+    // pwaAssets: {
+    //   config: true,
+    // },
+
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      navigateFallback: "/",
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          urlPattern: /.*\.(?:js|css|json|html)$/,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "assets-cache",
+            expiration: {
+              maxEntries: 20,
+              maxAgeSeconds: 60 * 60 * 24, // Cache por 1 día
+            },
+          },
+        },
+      ],
+    },
   },
 
-  compatibilityDate: '2024-12-30',
+  compatibilityDate: "2024-12-30",
 });
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Gabi Alvarez</h1>
+  </div>
+</template>
